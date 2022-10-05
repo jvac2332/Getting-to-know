90177d3407aca0f8f0962ba1b0569151ec9a2471
Joshua Vachachira <joshuavachachira@Joshuas-MacBook-Pro-2.local>
2022-10-05 11:25:43 -0500

    first commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+print("Joshua Vachachira Helllo World")
+
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow!",
+    "Curly": "Nyuk nyuk!",
+    }
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
+
